4a06dbb8ea1cf6b01e33261151bb5f0d90e50b72
JuanCarlosMarino <46898982+JuanCarlosMarino@users.noreply.github.com>
2020-10-19 19:29:18 -0500

    Add (Ship movement and Asteroid movement)

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float speed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.drag = 0;
+        rb.angularDrag = 0;
+
+        rb.velocity = new Vector3(
+            Random.Range(-1f,1f),
+            Random.Range(-1f,1f),
+            0
+        ).normalized*speed;
+        rb.angularVelocity = Random.Range (-50f, 50f);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float accerleration;
+
+    public float maxSpeed;
+
+    public float drag;
+
+    public float angularSpeed;
+
+    private Rigidbody2D rb;
+
+    private float vertical;
+
+    private float horizontal;
+
+    private bool shooting;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.drag = drag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = InputManager.Vertical;
+        horizontal = InputManager.Horizontal;
+        shooting = InputManager.Fire;
+
+        Rotate();
+    }
+
+    private void FixedUpdate()
+    {
+        var fowardMotor = Mathf.Clamp(vertical,0f,1f);
+        rb.AddForce(transform.up * accerleration * fowardMotor);
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+
+    private void Rotate()
+    {
+        if (horizontal == 0)
+        {
+            return;
+        }
+        transform.Rotate(0,0,-angularSpeed *horizontal *Time.deltaTime);
+    }
+}
